e0fa78dd95d44b476b8412fd2c5a61fbf6fa2d41
Muryllo Vieira <muryllovieira59@gmail.com>
2022-09-01 16:57:22 -0300

    primeiro commit

---
+++ src/br/senai/sp/jandira/model/Tabuada.java
@@ -0,0 +1,27 @@
+package br.senai.sp.jandira.model;
+
+public class Tabuada {
+
+	public int multiplicando;
+	public int minimoMultiplicador;
+	public int maximoMultiplicador;
+	
+	public void getNumeroMultiplicador() {
+		
+	}
+	public void getMaximoMultiplicador() {
+		
+	}
+	public void getMinimoMultiplicador() {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
