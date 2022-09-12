4047c756c0164c2bc5bde27aa71b4fdb74f18dcd
Muryllo Vieira <muryllovieira59@gmail.com>
2022-09-12 14:14:21 -0300

    finalizado

--- src/br/senai/sp/jandira/gui/FrameTabuada.java
+++ src/br/senai/sp/jandira/gui/FrameTabuada.java
@@ -29,126 +29,112 @@ public class FrameTabuada {
 	public Color corDoSegundoBotao;
 	public Font fonteDaLista;
 	public Color corDoFundo;
-	
+
 	public void criarTela() {
-		
+
 		JFrame tela = new JFrame();
 		tela.setTitle(titulo);
 		tela.setSize(largura, altura);
 		tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		tela.setLayout(null);
-		
+
 		Container painel = tela.getContentPane();
 		painel.setBackground(corDoFundo);
-		
+
 		JLabel labelTabuada = new JLabel();
 		labelTabuada.setText("Tabuada 1.0");
 		labelTabuada.setBounds(120, 10, 600, 30);
-		labelTabuada.setFont(new Font("Inter", Font.BOLD,25));
+		labelTabuada.setFont(new Font("Inter", Font.BOLD, 25));
 		labelTabuada.setForeground(Color.red);
-		
+
 		JLabel labelTexto = new JLabel();
-		labelTexto.setText("<html>Com a tabuada 1.0 os seus problemas acabaram.<br> Calcule a tabuada que desejar em segundos!</html>"
-				+ "");
+		labelTexto.setText(
+				"<html>Com a tabuada 1.0 os seus problemas acabaram.<br> Calcule a tabuada que desejar em segundos!</html>"
+						+ "");
 		labelTexto.setBounds(120, 45, 600, 30);
 		labelTexto.setFont(new Font("Calibri", Font.BOLD, 12));
-		
+
 		ImageIcon icone = new ImageIcon("src/br/senai/sp/jandira/img/questionario.png");
 		JLabel icon = new JLabel(icone);
 		icon.setBounds(10, 8, 100, 100);
-		
-		JLabel labelMultiplicando =  new JLabel();
+
+		JLabel labelMultiplicando = new JLabel();
 		labelMultiplicando.setText("Multiplicando:");
 		labelMultiplicando.setBounds(121, 100, 600, 30);
 		labelMultiplicando.setFont(fonteDosLabels);
 		labelMultiplicando.setForeground(Color.BLACK);
-		
+
 		JTextField textFieldMultiplicando = new JTextField();
 		textFieldMultiplicando.setBounds(250, 100, 200, 30);
 		textFieldMultiplicando.setFont(new Font("Arial", Font.BOLD, 25));
 		textFieldMultiplicando.setForeground(Color.BLUE);
 		textFieldMultiplicando.setHorizontalAlignment(JTextField.RIGHT);
-		
-		JLabel labelminimoMultiplicador =  new JLabel();
+
+		JLabel labelminimoMultiplicador = new JLabel();
 		labelminimoMultiplicador.setText("Mínimo Multiplicador:");
 		labelminimoMultiplicador.setBounds(70, 150, 600, 30);
 		labelminimoMultiplicador.setFont(fonteDosLabels);
 		labelminimoMultiplicador.setForeground(Color.BLACK);
-		
+
 		JTextField textFieldminimoMultiplicador = new JTextField();
 		textFieldminimoMultiplicador.setBounds(250, 150, 200, 30);
 		textFieldminimoMultiplicador.setFont(new Font("Arial", Font.BOLD, 25));
 		textFieldminimoMultiplicador.setForeground(Color.BLUE);
 		textFieldminimoMultiplicador.setHorizontalAlignment(JTextField.RIGHT);
-		
-		JLabel labelmaximoMultiplicador =  new JLabel();
+
+		JLabel labelmaximoMultiplicador = new JLabel();
 		labelmaximoMultiplicador.setText("Máximo Multiplicador:");
 		labelmaximoMultiplicador.setBounds(70, 200, 600, 30);
 		labelmaximoMultiplicador.setFont(fonteDosLabels);
 		labelmaximoMultiplicador.setForeground(Color.BLACK);
-		
+
 		JTextField textFieldmaximoMultiplicador = new JTextField();
 		textFieldmaximoMultiplicador.setBounds(250, 200, 200, 30);
 		textFieldmaximoMultiplicador.setFont(new Font("Arial", Font.BOLD, 25));
 		textFieldmaximoMultiplicador.setForeground(Color.BLUE);
 		textFieldmaximoMultiplicador.setHorizontalAlignment(JTextField.RIGHT);
-		
+
 		JButton buttonCalcular = new JButton();
 		buttonCalcular.setText("Calcular");
 		buttonCalcular.setBounds(70, 250, 200, 30);
 		buttonCalcular.setBackground(corDoBotao);
-		
+
 		JButton buttonLimpar = new JButton();
 		buttonLimpar.setText("Limpar");
 		buttonLimpar.setBounds(300, 250, 150, 30);
 		buttonLimpar.setBackground(corDoSegundoBotao);
-		
+
 		JLabel labelResultado = new JLabel();
 		labelResultado.setText("Resultado:");
 		labelResultado.setBounds(70, 290, 600, 30);
 		labelResultado.setFont(fonteDosLabels);
 		labelResultado.setForeground(Color.BLACK);
-		
+
 		JScrollPane scroll = new JScrollPane();
 		scroll.setBounds(70, 320, 470, 220);
 		JList<String> listLista = new JList<>();
 		listLista.setBackground(new Color(255, 255, 167));
 		listLista.setFont(fonteDaLista);
 		listLista.setBorder(new LineBorder(Color.BLUE));
-		
-		buttonCalcular.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				Tabuada tabuada = new Tabuada();
-				tabuada.multiplicando = Integer.parseInt(textFieldMultiplicando.getText());
-				tabuada.minimoMultiplicador = Integer.parseInt(textFieldminimoMultiplicador.getText());
-				tabuada.maximoMultiplicador = Integer.parseInt(textFieldmaximoMultiplicador.getText());
-				
-				listLista.setListData(tabuada.getTabuada());
-				scroll.getViewport().add(listLista);
-			
-			}
-		});
-		
+
+
 		buttonLimpar.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if(e.getSource() == buttonLimpar );
-				
+				if (e.getSource() == buttonLimpar)
+					;
+
 				textFieldMultiplicando.setText(null);
 				textFieldminimoMultiplicador.setText(null);
 				textFieldmaximoMultiplicador.setText(null);
-				
+
 				DefaultListModel<String> lista = new DefaultListModel<>();
 				listLista.setModel(lista);
-				
+
 			}
 		});
-		
-		
-		
+
 		painel.add(labelTabuada);
 		painel.add(labelTexto);
 		painel.add(icon);
@@ -163,57 +149,50 @@ public class FrameTabuada {
 		painel.add(labelResultado);
 		painel.add(scroll);
 		painel.add(listLista);
-		
-		
-		
+
 		tela.setVisible(true);
-		
+
 		buttonCalcular.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
-				Tabuada tabuada = new Tabuada();
-				tabuada.multiplicando = Integer.parseInt(textFieldMultiplicando.getText());
-				tabuada.minimoMultiplicador = Integer.parseInt(textFieldminimoMultiplicador.getText());
-				tabuada.maximoMultiplicador = Integer.parseInt(textFieldmaximoMultiplicador.getText());
-				if(tabuada.maximoMultiplicador < tabuada.minimoMultiplicador) {
-					JOptionPane.showMessageDialog(null, "Multiplicador mínimo maior que o máximo", "ERRO", JOptionPane.OK_OPTION);
-					textFieldMultiplicando.setText(null);
-					textFieldminimoMultiplicador.setText(null);
-					textFieldmaximoMultiplicador.setText(null);
-					
+
+				if (textFieldMultiplicando.getText().isEmpty()) {
+					JOptionPane.showMessageDialog(null, "O multiplicando é obrigatório!");
+					textFieldMultiplicando.requestFocus();
+				} else if (textFieldminimoMultiplicador.getText().isEmpty()) {
+					JOptionPane.showMessageDialog(null, "O mínimo multiplicador é obrigatório!");
+					textFieldminimoMultiplicador.requestFocus();
+				} else if (textFieldmaximoMultiplicador.getText().isEmpty()) {
+					JOptionPane.showMessageDialog(null, "O máximo multiplicador é obrigatório!");
+					textFieldmaximoMultiplicador.requestFocus();
+				} else if(Integer.parseInt(textFieldMultiplicando.getText()) > 1000 || Integer.parseInt(textFieldminimoMultiplicador.getText()) > 1000 || Integer.parseInt(textFieldmaximoMultiplicador.getText()) > 1000) {
+					JOptionPane.showMessageDialog(null, "Os valores devem ser menor ou igual a 1000!", "ERRO", JOptionPane.OK_OPTION);
 				} else {
-				
-				listLista.setListData(tabuada.getTabuada());
-				scroll.getViewport().add(listLista);
-				
+					
+					Tabuada tabuada = new Tabuada();
+					tabuada.multiplicando = Integer.parseInt(textFieldMultiplicando.getText());
+					tabuada.minimoMultiplicador = Integer.parseInt(textFieldminimoMultiplicador.getText());
+					tabuada.maximoMultiplicador = Integer.parseInt(textFieldmaximoMultiplicador.getText());
+					if(tabuada.maximoMultiplicador < tabuada.minimoMultiplicador) {
+						JOptionPane.showMessageDialog(null, "Multiplicador mínimo maior que o máximo", "ERRO", JOptionPane.OK_OPTION);
+						textFieldMultiplicando.setText("");
+						textFieldminimoMultiplicador.setText("");
+						textFieldmaximoMultiplicador.setText("");
+					} else {
+						
+						listLista.setListData(tabuada.getTabuada());
+						scroll.getViewport().add(listLista);
+						
+					}
 				}
 				
+				System.out.println("Saiu!!");
+
+
 			}
 		});
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
 	}
-	
-	
+
 }
